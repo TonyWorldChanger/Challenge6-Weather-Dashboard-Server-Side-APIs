aff0dafb9c3fb8b06af7ccabd1df6c1e59c1295c
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-15 14:27:29 -0600

    adding links to html for css and bootstrap

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var zipCode = "75019";
 var apiKey = "9245a40f3fa9510a8e08caac843d31d3";
-var apiPath = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+var apiPath = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}`;
+
+
+
+function getForecast() {
+    fetch(apiPath).then((res) => {
+        return res.json()
+    }).then((json) => {
+        console.log(json);
+    }).catch((err) => {
+        console.log(err.message)
+    })
+
+}
+getForecast(zipCode, apiKey);
