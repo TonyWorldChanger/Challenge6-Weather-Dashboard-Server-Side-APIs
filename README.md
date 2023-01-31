37d9b69f0577829a81311f516413459a572afa0b
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-30 18:22:12 -0600

    fetch api weather

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Challenge6-Weather-Dashboard-Server-Side-APIs
+
+
+
+
+
+
+
+## Credit
+Mykel Valadez YouTube channel
--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-var zipCode = "75019";
+
 var apiKey = "9245a40f3fa9510a8e08caac843d31d3";
-var apiPath = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}`;
+var searchButton = document.querySelector("#searchBtn");
 
 
 
-function getForecast() {
-    fetch(apiPath).then((res) => {
-        return res.json()
-    }).then((json) => {
-        console.log(json);
-    }).catch((err) => {
-        console.log(err.message)
-    })
+$("#searchBtn").on("click", searchCity);
+    console.log("I work");
+    function searchCity() {
+        var userInput = $(this).parent().attr("id");
+        var userValue = $(this).parent().children().eq(1).val();
+        getCityName(userValue);
+        localStorage.setItem(userInput, userValue);
+        console.log("search");
+    }
 
-}
-getForecast(zipCode, apiKey);
+
+
+
+function getCityName(cityName) {
+    var apiPath = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+      fetch(apiPath).then((res) => {
+          return res.json()
+      }).then((json) => {
+          console.log(json);
+          let lat = json[0].lat;
+          let lon = json[0].lon;
+          getweather(lat, lon);
+      }).catch((err) => {
+          console.log(err.message);
+      })
+  
+  
+  };
+
+
+ function getweather(lat, lon) {
+    var apiPath =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(apiPath).then((res) => {
+            return res.json()  
+        }).then((json) => {
+            console.log(json);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        })
+
+ };
+
+ 
